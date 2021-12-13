7da117479616e1c446a87e8e3d8912c5bc729da9
RedBrumbler <redbrumbler.official+github@gmail.com>
2021-12-13 23:15:06 +0100

    Make it work properly

--- build.ps1
+++ build.ps1
@@ -1,7 +1,18 @@
 Param(
     [Parameter(Mandatory=$false)]
+    [Switch]$clean,
+    [Parameter(Mandatory=$false)]
     [Switch]$release
 )
+
+# if user specified clean, remove all build files
+if ($clean.IsPresent)
+{
+    if (Test-Path -Path "build")
+    {
+        remove-item build -R
+    }
+}
 
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
@@ -13,7 +24,11 @@ if ($release.IsPresent)
     $BUILD_TYPE = "Release"
 }
 
-mkdir build
+if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
+{
+    mkdir build
+} 
+
 cd build
 $cmd = "cmake -G ""Ninja"" -DCMAKE_BUILD_TYPE=`"$BUILD_TYPE`" ../"
 Invoke-Expression $cmd
--- include/config.hpp
+++ include/config.hpp
@@ -1,23 +1,18 @@
 #pragma once
-#include "../extern/beatsaber-hook/shared/config/rapidjson-utils.hpp"
-#include "../extern/beatsaber-hook/shared/config/config-utils.hpp"
+#include "beatsaber-hook/shared/config/config-utils.hpp"
+#include "beatsaber-hook/shared/config/rapidjson-utils.hpp"
 #include "modloader/shared/modloader.hpp"
 #include <string>
-typedef enum _shape_t {
-    CUBE = 0,
-    CYLINDER = 1
-} shape_t;
-
+typedef enum _shape_t { CUBE = 0, CYLINDER = 1 } shape_t;
 
 typedef struct _config_t {
-    bool enabled = true;
-    double xmin = -2.0;
-    double xmax = 2.0;
-    double zmin = -2.0;
-    double zmax = 8.0;
-    int shape = 0;
+  bool enabled = true;
+  double xmin = -2.0;
+  double xmax = 2.0;
+  double zmin = -2.0;
+  double zmax = 8.0;
+  int shape = 0;
 } config_t;
-
 
 Configuration& getConfig();
 void SaveConfig();
